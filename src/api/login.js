78d13b774f8efa1fa24d200cf6ed4ff838dc258f
haitaodesign <1775814590@qq.com>
2017-08-17 10:53:23 +0800

    提交一次更改

---
+++ src/api/login.js
@@ -0,0 +1,14 @@
+// 通过封装axios和调用mock数据，统一管理api接口数据
+import fetch from './utils/axios.js'
+
+export function Login(username,password){
+    const data = {
+        username,
+        password
+    };
+    return fetch({
+        url:'/Login',
+        method:'post',
+        data
+    });
+}
